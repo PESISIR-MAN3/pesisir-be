a2b3327ccd34724d055295f3fbc6b89f6e4d9651
ee-son <isan.faizun.hidar@mail.ugm.ac.id>
2025-09-24 14:35:11 +0700

    add donation method seeder

---
+++ database/seeders/DonationMethodSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DonationMethod;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DonationMethodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DonationMethod::updateOrCreate(
+             ['method_name' => 'BSI'],
+            [
+                'account_number' => '7327281881',
+                'owner_name'     => 'Arum Sekar Waradanti',
+            ]
+        );
+
+        DonationMethod::updateOrCreate(
+            ['method_name' => 'BNI'],
+            [
+                'account_number' => '1980326177',
+                'owner_name'     => 'Nirwasita Indrani',
+            ]
+        );
+    }
+}
